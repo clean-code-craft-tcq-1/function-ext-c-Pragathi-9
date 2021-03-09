216348fa106db7d225e86b710d8d6a348ae22d62
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-03-09 22:52:46 +0530

    Update checker.c
    Added macros to the header file

--- checker.c
+++ checker.c
@@ -1,24 +1,10 @@
 /* Include Headerfiles													*/
 /* ===================													*/
 
-
 #include <stdio.h>
 #include <assert.h>	
 #include <stdbool.h>
-/* ===================													*/
-/* Macros and define values */
-/* ===================													*/
-#define MAXCHARGERATE 0.5
-#define MAXTEMP 45.0
-#define MINTEMP 0
-#define MAXSOC 80.0
-#define MINSOC 20.0
-
-/****************************************************************************
-Function declaration
-/***************************************************************************/
-int batteryIsOk(float StateofHealth, float ChargeRate, float stateofcharge, float temperature);
-
+#include <checker.h>
 /********************************************************************************
  * A function that gives the State-of -Health status of a battery management system.
  * if the current SOH rating us below the threshold 0.5%, then the battery is unacceptable.
