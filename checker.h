c111366243be33201d4646cb462498d0196bd08c
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-03-11 00:37:38 +0530

    Update checker.h
    Addition of warning levels

--- checker.h
+++ checker.h
@@ -12,6 +12,21 @@
 #define MAXSOC 80.0
 #define MINSOC 20.0
 
+const char* StateofCharge[] = {
+" Warning: State-of-Charge low level breahced", 
+"Warning: State-of-Charge level low",
+ " State-of-Charge level NORMAL", 
+"Warning: State-of-Charge level High",
+ "Warning: State-of-Charge high level breached"
+};
+
+const char* TemperatureStatus[] = {
+	" Warning: Temperature low level breahced", 
+"Warning: Temperature level low",
+ " Temperature is NORMAL, and within the ideal range", 
+"Warning: Temperature level High",
+ "Warning: Temperature high level breached"
+};
 /****************************************************************************
 Function declaration
 /***************************************************************************/
