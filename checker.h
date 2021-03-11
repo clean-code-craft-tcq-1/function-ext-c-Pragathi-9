7a03ec22cd6983627b6f782ddf265504d08f064d
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-03-11 17:44:11 +0530

    Update checker.h
    German text added

--- checker.h
+++ checker.h
@@ -12,7 +12,15 @@
 #define MAXSOC 80.0
 #define MINSOC 20.0
 
-const char* StateofCharge[] = {
+
+const char* StateofChargeinGerman[] = {
+" Warnung: Der Ladezustand ist niedrig", 
+"Warnung: Ladezustand niedrig",
+ " Ladezustand NORMAL", 
+"Warnung: Ladezustand hoch",
+ "Warnung: High-Level des Ladezustands verletzt"
+};
+const char* StateofChargeinEnglish[] = {
 " Warning: State-of-Charge low level breahced", 
 "Warning: State-of-Charge level low",
  " State-of-Charge level NORMAL", 
@@ -20,12 +28,29 @@ const char* StateofCharge[] = {
  "Warning: State-of-Charge high level breached"
 };
 
-const char* TemperatureStatus[] = {
-	" Warning: Temperature low level breahced", 
+const char* TemperatureStatusEnglish[] = {
+" Warning: Temperature low level breahced", 
 "Warning: Temperature level low",
  " Temperature is NORMAL, and within the ideal range", 
 "Warning: Temperature level High",
  "Warning: Temperature high level breached"
+};
+const char* TemperatureStatusGerman[] = {
+" Warnung: Temperatur niedrig durchbrochen", 
+"Warnung: Temperaturniveau niedrig ",
+ " Die Temperatur ist NORMAL und liegt im idealen Bereich", 
+"Warnung: Temperaturstufe hoch",
+ "Warnung: Hohe Temperatur verletzt"
+};
+
+const char* BMSGoodStatus[]={
+	"Das Batteriemanagementsystem ist unter Berücksichtigung der oben genannten Faktoren in gutem Zustand \ n",
+	"The Battery management system is in good condition considering the above factors \n"
+};
+
+const char* BMSPoorStatus[]={
+	"Das Batteriemanagementsystem ist unter Berücksichtigung der oben genannten Faktoren in einem schlechten Zustand. \n",	
+	"The Battery management system is in bad condition considering the above factors \n"
 };
 /****************************************************************************
 Function declaration
