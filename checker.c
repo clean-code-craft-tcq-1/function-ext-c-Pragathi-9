944d1a1c921f8dd41c789d5b66d2ac4d07791ac9
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-03-17 15:52:18 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -103,9 +103,9 @@ int main()
 { 
     
     language=English;
-    float arr[][NumberOfParameters] = {{40, 0.5, 0.3}, {46, 0.9,0.6}, {30, 0.7, 0.2}}; 
+    float array[][NumberOfParameters] = {{40, 0.5, 0.3}, {46, 0.9,0.6}, {30, 0.7, 0.2}}; 
     bool resultant[NumberOfBatteries]={1,0,1};
-    assert(Accumulator(arr,resultant));
+    assert(!(Accumulator(array,resultant)));
     BatteryReport();
     return 0; 
 }
