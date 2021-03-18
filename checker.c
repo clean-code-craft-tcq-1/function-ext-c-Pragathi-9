eb94d2a9a30f7482fba70f804ec059a4f42754b7
Pragathi P Nayak <77482032+Pragathi-9@users.noreply.github.com>
2021-03-18 20:03:51 +0530

    Update checker.c
    Added another function to display the data other than controller
    resultant added as property in structure
    passing structure instead of variables

--- checker.c
+++ checker.c
@@ -17,7 +17,8 @@
 bool BMS_AttributeinRangeStatus(float parametervalue, int index)
 
 {	bool InRange=true;
-	if ((parametervalue< (AttributeMinimumthreshold[index]))||(parametervalue>= (AttributeMaximumthreshold[index])))
+	
+ if((AttributeMaximumthreshold[index])<=parametervalue<(AttributeMinimumthreshold[index]))
 	  {		
 		  return (InRange=false);
 	  }
@@ -30,9 +31,9 @@ bool BMS_AttributeinRangeStatus(float parametervalue, int index)
  * input: Array which has parameter values of different batteries.
  * returns: Verification of the checked Battery status against the expected.
  *********************************************************************************/
-int BMS_AttributeStatusAccumulator(float Input_Attribute[NumberOfBatteries][NumberOfParameters],  bool Expected_result[])
+properties BMS_AttributeStatusAccumulator(float Input_Attribute[NumberOfBatteries][NumberOfParameters],  bool Expected_result[])
 {
-	int VerifyResult=0,VerifiedResult=1;
+	int VerifyResult=0;
 	struct BatteryProperties properties;
 	int BatteryIndex,ParameterIndex;
 	for (BatteryIndex=0;BatteryIndex < NumberOfBatteries;BatteryIndex++)
@@ -49,36 +50,40 @@ int BMS_AttributeStatusAccumulator(float Input_Attribute[NumberOfBatteries][Numb
 		}
 		
 		properties.BatteryStatus[BatteryIndex]= Battery_status;
-		VerifyResult= (!((properties.BatteryStatus[BatteryIndex]) ^ (Expected_result[BatteryIndex])));
-		VerifiedResult &= VerifyResult;
+		VerifiedResultfromBatteries= (!((properties.BatteryStatus[BatteryIndex]) ^ (Expected_result[BatteryIndex])));
+		properties.VerifiedResultfromBatteries &= VerifyResult;
 	}
-	
-	return (VerifiedResult);
+	BMS_Report(properties);
+	return (properties);
 }
-/********************************************************************************
- * This function calls the controller to report the battery parameter status
- *  It also displays the overall status of a battery, both in German and English as requested.
- *********************************************************************************/	
-void BMS_ReportingController()
-{	
-	
-	struct BatteryProperties properties;
-	
-	printf("Reporting the BMS health results from Controller as follows:\n");
-	
+
+void BMS_Report( struct BatteryProperties Displayproperties)
+{
 	int ParameterIndex,BatteryIndex;
 	for (BatteryIndex=0;BatteryIndex<NumberOfBatteries;BatteryIndex++)
 	{
 		for (ParameterIndex=0;ParameterIndex<NumberOfParameters;ParameterIndex++)
 			
 		{	
-			printf("%s = %f, %s \n", BMS_AttributeDisplay[language][ParameterIndex],properties.AttributeValue[BatteryIndex][ParameterIndex], BMS_AttributeRangeStatusDisplay[language][(properties.AttributeinRangeStatus[BatteryIndex][ParameterIndex])]);
+			printf("%s = %f, %s \n", BMS_AttributeDisplay[language][ParameterIndex],Displayproperties.AttributeValue[BatteryIndex][ParameterIndex], BMS_AttributeRangeStatusDisplay[language][(Displayproperties.AttributeinRangeStatus[BatteryIndex][ParameterIndex])]);
 			
 		}
 		
-		printf ("Battery %d -> %s \n",BatteryIndex,BMS_StatusDisplay[language][properties.BatteryStatus[BatteryIndex]]);
+		printf ("Battery %d -> %s \n",BatteryIndex,BMS_StatusDisplay[language][Displayproperties.BatteryStatus[BatteryIndex]]);
 		
 	}
+}
+	
+/********************************************************************************
+ * This function calls the controller to report the battery parameter status
+ *  It also displays the overall status of a battery, both in German and English as requested.
+ *********************************************************************************/	
+void BMS_ReportingController(struct BatteryProperties PropertiesToController;)
+{	
+	
+	
+	printf("Reporting the BMS health results from Controller as follows:\n");
+	BMS_Report(PropertiesToController);
 	
 	
 }
@@ -86,17 +91,19 @@ void BMS_ReportingController()
   
 int main() 
 { 
-    
+    struct BatteryProperties Accumulatedproperties;
     language=English;
     float SampleArray_1[][NumberOfParameters] = {{40, 0.2, 30}, {46, 0.3,80}, {30, 0.4, 40}}; 
     bool Expectedresultant_1[]={1,0,1};
-    assert((BMS_AttributeStatusAccumulator(SampleArray_1,Expectedresultant_1)));
-    BMS_ReportingController();
+    Accumulatedproperties=(BMS_AttributeStatusAccumulator(SampleArray_1,Expectedresultant_1));
+    assert(Accumulateproperties.VerifiedResultfromBatteries);
+    BMS_ReportingController(Accumulateproperties);
     
     language=German;
     float SampleArray_2[][NumberOfParameters] = {{50, 0.4, 60},{10, 0.6,25},{20, 0.25, 50}}; 
     bool Expectedresultant_2[]= {0,0,1};
-    assert(BMS_AttributeStatusAccumulator(SampleArray_2,Expectedresultant_2));
-    BMS_ReportingController();
+    Accumulatedproperties=(BMS_AttributeStatusAccumulator(SampleArray_2,Expectedresultant_2));
+    assert(Accumulatedproperties.VerifiedResultfromBatteries);
+    BMS_ReportingController(Accumulatedproperties);
     return 0; 
 }
